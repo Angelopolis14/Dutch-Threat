05cb26de58e1227bfdcff7bfd17c264b6e2cf3e9
Angel Garcia <angelo.1234.ag@gmail.com>
2023-01-19 15:32:26 -0600

    First Module of Angular

--- client/dist/out-tsc/app/app.module.js
+++ client/dist/out-tsc/app/app.module.js
@@ -2,12 +2,14 @@ import { __decorate } from "tslib";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import ProductListView from './views/productListView.component';
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     NgModule({
         declarations: [
-            AppComponent
+            AppComponent,
+            ProductListView
         ],
         imports: [
             BrowserModule
---
+++ client/src/app/views/productListView.component.ts
@@ -0,0 +1,15 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    selector: "product-list",
+    templateUrl: "productListView.component.html"
+})
+export default class ProductListView {
+    public products = [{
+        title: "Nike Air Force 1",
+        price: "2000.00"
+    }, {
+        title: "Jordan 4",
+        price: "5000.00"
+    }];
+}
