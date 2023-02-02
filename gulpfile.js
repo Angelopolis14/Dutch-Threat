e3a2369c8768a9f8b057297f98b02794661b1dd3
Angel Garcia <angelo.1234.ag@gmail.com>
2023-02-02 15:18:34 -0600

    manifiyin

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+﻿var gulp = require("gulp");
+var uglify = require("gulp-uglify"); 
+var concat = require("gulp-concat");
+
+//minify JavaScript
+function minify() {
+    return gulp.src(["wwwroot/js/**/*.js"])
+        .pipe(uglify())
+        .pipe(concat("dutchtreat.min.js"))
+        .pipe(gulp.dest(("wwwroot/dist/"));
+}
+//minify CSS
+
+
+
+
+exports.default = minify;
